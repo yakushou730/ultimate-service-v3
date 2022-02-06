129e6bde9ba807f4736f75a32222ec13c0ae8bf7
Shou Tseng <yakushou730@gmail.com>
2022-02-06 15:28:50 +0800

    feat: request context

--- business/web/mid/logger.go
+++ business/web/mid/logger.go
@@ -15,17 +15,20 @@ func Logger(log *zap.SugaredLogger) web.Middleware {
 	m := func(handler web.Handler) web.Handler {
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
-			traceID := "0000000000000000"
-			statusCode := http.StatusOK
-			now := time.Now()
+			// If the context is missing this value, request the service
+			// to be shutdown gracefully.
+			v, err := web.GetValues(ctx)
+			if err != nil {
+				return err // web.NewShutdownError("web value missing from context")
+			}
 
-			log.Infow("request started", "traceid", traceID, "method", r.Method, "path", r.URL.Path,
+			log.Infow("request started", "traceid", v.TraceID, "method", r.Method, "path", r.URL.Path,
 				"remoteaddr", r.RemoteAddr)
 
-			err := handler(ctx, w, r)
+			err = handler(ctx, w, r)
 
-			log.Infow("request completed", "traceid", traceID, "method", r.Method, "path", r.URL.Path,
-				"remoteaddr", r.RemoteAddr, "statuscode", statusCode, "since", time.Since(now))
+			log.Infow("request completed", "traceid", v.TraceID, "method", r.Method, "path", r.URL.Path,
+				"remoteaddr", r.RemoteAddr, "statuscode", v.StatusCode, "since", time.Since(v.Now))
 
 			return err
 		}
